a1427ccffe633f05a568223507fa99855ec376ad
Jansen Howell <jansenhowell@yahoo.com>
2020-03-01 21:08:42 -0600

    added appropriate buttons to the menu

---
+++ app/src/main/java/com/example/sign_lang_ml/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sign_lang_ml;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
