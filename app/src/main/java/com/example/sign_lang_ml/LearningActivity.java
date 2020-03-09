e35a364429b909dadd30796f43c8c4cfc7feecde
Jansen Howell <jansenhowell@yahoo.com>
2020-03-09 18:11:00 -0500

    added the letters menu

--- app/src/main/java/com/example/sign_lang_ml/LearningActivity.java
+++ app/src/main/java/com/example/sign_lang_ml/LearningActivity.java
@@ -2,13 +2,41 @@ package com.example.sign_lang_ml;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LearningActivity extends AppCompatActivity {
+
+    ListView listview;
+    List list = new ArrayList();
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
+
+        listview = (ListView)findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter =  new ArrayAdapter<String>(LearningActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.letters));
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+                Intent intent = new Intent(LearningActivity.this, LearningLettersActivity.class);
+                intent.putExtra("LetterName", listview.getItemAtPosition(i).toString());
+                startActivity(intent);
+            }
+        });
+
+        listview.setAdapter(adapter);
+
     }
 }
