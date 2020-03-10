552d821755d412ad77edeebbde482a47af324b14
Jansen Howell <jansenhowell@yahoo.com>
2020-03-09 20:04:43 -0500

    successfully connected OpenCV

--- app/src/main/java/com/example/sign_lang_ml/MainActivity.java
+++ app/src/main/java/com/example/sign_lang_ml/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
