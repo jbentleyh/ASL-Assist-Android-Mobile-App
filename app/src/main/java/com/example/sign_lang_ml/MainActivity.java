81eb953ba448596e5cddcc89712aa6f2d9318b26
Jansen Howell <jansenhowell@yahoo.com>
2020-04-12 20:37:37 -0500

    design changes for tha main menu and added a logo

--- app/src/main/java/com/example/sign_lang_ml/MainActivity.java
+++ app/src/main/java/com/example/sign_lang_ml/MainActivity.java
@@ -30,28 +30,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        // Training Button
-        btn = findViewById(R.id.training);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), TrainingActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        // Speech-To-Text Button
-        btn = findViewById(R.id.speechtotext);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SpeechToTextActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
         // Learning Button
         btn = findViewById(R.id.learning);
         btn.setOnClickListener(new View.OnClickListener() {
