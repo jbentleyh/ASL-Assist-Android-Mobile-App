2dd2335814646385e486f71d122ab1ac7bfb7e3f
Joseph Daughdrill <joseph.daughdrill@usm.edu>
2020-04-22 19:52:05 -0500

    App now requests camera permission

--- app/src/main/java/com/example/sign_lang_ml/MainActivity.java
+++ app/src/main/java/com/example/sign_lang_ml/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sign_lang_ml;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,9 +18,33 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn;
 
+    private final static int MY_PERMISSIONS_REQUEST_CAMERA = 12;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
         setContentView(R.layout.activity_main);
 
         // Recognition Button
